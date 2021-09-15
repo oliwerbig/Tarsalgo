ca375a1b69c25c9db4e14f89122852faa108b8c4
Nagy Olivér Emmanuel <Nagy.Oliver@bpg.hu>
2021-09-15 08:41:25 +0200

    Add project files.

---
+++ tarsalgo/Event.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarsalgo
+{
+    class Event
+    {
+        private int entityId { get; set; } = 0;
+        private Direction direction = Direction.In;
+
+        public Event(string payload)
+        {
+            parsePayload(payload);
+        }
+
+        string getDirectionAsString()
+        {
+            return directionToString[getDirection()];
+        }
+
+        void setDirectionFromString(string directionString)
+        {
+            if (directionString == directionToString[In])
+            {
+                setDirection(Direction.In);
+            }
+            else if (directionString == directionToString[Out])
+            {
+                setDirection(Direction.Out);
+            }
+        }
+
+        void parsePayload(string payload)
+        {
+            char splitDelimiter = ' ';
+            List<string> values = new List<string>();
+            string[] items = payload.Split(' ');
+
+            foreach(string item in items)
+            {
+                values.Add(item);
+            }
+
+            setTime(stoi(values[0]), stoi(values[1]));
+            setEntityId(stoi(values[2]));
+            setDirectionFromString(values[3]);
+        }
+    }
+}
---
+++ tarsalgo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tarsalgo
+{
+    class Program
+    {
+
+        List<Event> events = new List<Event>();
+        Dictionary<int, Entity> entities = new Dictionary<int, Entity>();
+        Dictionary<int, TimeStamp> timeStamps = new Dictionary<int, TimeStamp>();
+
+        static void Main(string[] args)
+        {
+            processFile(@"ajto.txt");
+
+            Console.WriteLine("2. feladat");
+            Console.WriteLine("Az első belépő: " + "");
+            Console.WriteLine("Az utolsó belépő: " + "");
+            Console.WriteLine("");
+
+            /*writeEntityAndNumOfEventsToFile();
+
+            Console.WriteLine("");
+            Console.WriteLine("4. feladat");
+            Console.WriteLine("A végén a társalgóban voltak: ");
+            Dictionary<int, Entity> entitiesStillIn = findEntitiesStillIn()
+                Console.WriteLine("");*/
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void processFile(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+        
+            foreach (string line in lines)
+            {
+                Event event(line);
+                events.add(event);
+            }
+        }
+    }
+}
